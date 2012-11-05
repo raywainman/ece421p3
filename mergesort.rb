541ccee61cf9a76af15e20de6971ca247a7b759f
Raymond Wainman <raywainman@gmail.com>
2012-11-05 14:45:57 -0700

    Merge module skeleton

---
+++ mergesort.rb
@@ -0,0 +1,21 @@
+module MergeSort {
+	def sort(comparator = DefaultComparator.new(), timeout = 1000) {
+
+	}
+
+	def psort(astart, aend, bstart, bend, p, q, comparator) {
+
+	}
+
+	def binary_search(a, b) {
+		# A[], B[]
+	}
+
+	def merge(astart, aend, bstart, bend, p, q, comparator) {
+
+	}
+
+	def pmerge(astart, aend, bstart, bend, p, q, comparator) {
+
+	}
+}
