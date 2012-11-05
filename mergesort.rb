a6dbb0f9828f921e5e7da091c948b22b0bbf2847
Raymond Wainman <raywainman@gmail.com>
2012-11-05 15:15:37 -0700

    Fixing my ruby/java hybrid stupidness :P

--- mergesort.rb
+++ mergesort.rb
@@ -1,21 +1,18 @@
-module MergeSort {
-	def sort(comparator = DefaultComparator.new(), timeout = 1000) {
+module MergeSort
+	def sort(comparator = DefaultComparator.new(), timeout = 1000)
+	end
 
-	}
+	def psort(astart, aend, bstart, bend, p, q, comparator)
+	end
 
-	def psort(astart, aend, bstart, bend, p, q, comparator) {
-
-	}
-
-	def binary_search(a, b) {
+	def binary_search(a, b)
 		# A[], B[]
-	}
+	end
 
-	def merge(astart, aend, bstart, bend, p, q, comparator) {
+	def merge(astart, aend, bstart, bend, p, q, comparator)
+	end
 
-	}
+	def pmerge(astart, aend, bstart, bend, p, q, comparator)
 
-	def pmerge(astart, aend, bstart, bend, p, q, comparator) {
-
-	}
-}
+	end
+end
